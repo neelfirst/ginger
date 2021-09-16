158f248716d3a099b74d7bb340ffdc9b83273303
neelfirst <neelfirst@gmail.com>
2021-09-16 16:24:27 -0700

    first attempt to display a plot

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 from flask import Flask
+from tradier import *
+from utils import *
+
+WIDTH = 100 # number of data points to chart
 
 app = Flask(__name__)
 
+def generatePage(name, image):
+  string = "<html><head><title>"+name+"</title></head><body><img src="+image+"></body></html>
+  return(string)
+
 @app.route("/")
 def hello():
-  return 'hello world'
+  return('there is no spoon')
 
 @app.route("/<string:name>/")
 def say_hello(name):
-  return f"hello {name}"
+  start = (datetime.now() - timedelta(days=WIDTH)).date
+  stop = datetime.now().date
+  stock_data = getStockHistory(name, start, stop, interval='daily')
+  stock_plot = getStockPlot(stock_data, name, 'date', 'close', volume="no")
+  stock_page = generatePage(name, stock_plot)
+  return stock_page
 
 if __name__ == "__main__":
   app.run()
