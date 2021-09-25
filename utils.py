dc463fd935e411302a6e01018559b4711fd030fb
neelfirst <neelfirst@gmail.com>
2021-09-25 17:00:40 +0000

    add volume

--- utils.py
+++ utils.py
@@ -26,6 +26,9 @@ def getStockPlot(data, name='', volume='no'):
   df = df.set_index('date')
 
   buf = io.BytesIO()
-  mpf.plot(df, type='candle', figratio=(16.00,5.75), savefig=buf)
+  if (volume == 'yes'):
+    mpf.plot(df, type='candle', figratio=(16.00,8.00), , volume=True, savefig=buf)
+  else:
+    mpf.plot(df, type='candle', figratio=(16.00,5.75), savefig=buf)
   buf.seek(0)
   return buf
